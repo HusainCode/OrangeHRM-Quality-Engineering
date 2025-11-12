b7cab5cb74aebeaf0766426941f3937eed642dc6
HusainCode <alshaikhahmed@hotmail.com>
2025-11-11 22:32:45 -0600

    strted test login

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,15 +1,30 @@
 from playwright.sync_api import sync_playwright, expect
 
 
-class OrangeHRMLoginTest:
-    def __init__(self):
+class TestOrangeHRMLogin:
+    def setup_method(self):
         self.url = "https://opensource-demo.orangehrmlive.com/"
         self.username = "Admin"
         self.password = "admin123"
 
-    def run_test(self, headless=False):
-        pass
+    def test_login(self, headless=True):
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
 
+            page.goto(self.url)
+
+            page.get_by_placeholder("Username").fill(self.password)
+            page.get_by_placeholder("Password").fill(self.password)
+            page.get_by_role("button", name="Login").click()
+
+            page.wait_for_selector("text=Dashboard")
+            expect(page.locator("text=Dashboard")).to_be_visible()
+
+            print("Login test passed successfully!")
+
+            browser.close()
+            
 
 if __name__ == "__main__":
-    OrangeHRMLoginTest().run_test(headless=False)
+    TestOrangeHRMLogin().test_login(headless=False)
