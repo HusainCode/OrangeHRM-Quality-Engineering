b3b5ffb6aef1ff30f771f1ea889837415ce188f0
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-11-14 14:33:04 -0600

    Add login functionality and locators to login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,6 +7,33 @@ class LoginPage:
     def __init__(self, page: Page):
         self.page = page
 
+    # ---------- Navigation ----------
     def navigate(self):
         self.page.goto(self.URL)
+
+    # ---------- Locators ----------
+    @property
+    def username_input(self):
+        return self.page.get_by_placeholder("Username")
     
+    @property
+    def password_input(self):
+        return self.page.get_by_placeholder("Password")
+    
+    @property
+    def login_button(self):
+        return self.page.get_by_role("button", name="Login")
+    
+    @property
+    def error_message(self):
+        return self.page.get_by_text("Invalid credentials")
+    
+    # ---------- Actions ----------
+    def login(self, username: str, password:str):
+        self.username_input.fill(username)
+        self.password_input.fill(password)
+        self.login_button.click()
+    
+    # ---------- Assertions ----------
+    def assert_login_error(self):
+        expect(self.error_message).to_be_visible()
