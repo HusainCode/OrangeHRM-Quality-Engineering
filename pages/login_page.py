ed8b215c7c3e6a45b84f5306b046ed2cc7b095a7
HusainCode <alshaikhahmed@hotmail.com>
2025-11-17 19:04:59 -0600

    Minor Change

--- pages/login_page.py
+++ pages/login_page.py
@@ -42,6 +42,7 @@ class LoginPage:
     def login(self, username: str, password: str):
         self.username_input.fill(username)
         self.password_input.fill(password)
+        self.page.wait_for_load_state("networkidle")
         self.login_button.click()
 
     def get_error_text(self):
