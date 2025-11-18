6115ce6a7c2b07c0ec0285d86259b1e1f64d443c
HusainCode <alshaikhahmed@hotmail.com>
2025-11-17 18:44:44 -0600

    more login tests

--- pages/login_page.py
+++ pages/login_page.py
@@ -30,12 +30,33 @@ class LoginPage:
             ".oxd-alert-content-text, .oxd-input-field-error-message"
         ).first
 
+    @property
+    def forgot_password_link(self):
+        return self.page.get_by_text("Forgot your password?")
+
+    @property
+    def page_title(self):
+        return self.page.locator(".oxd-text--h5")
+
     # ---------- Actions ----------
     def login(self, username: str, password: str):
         self.username_input.fill(username)
         self.password_input.fill(password)
         self.login_button.click()
 
+    def get_error_text(self):
+        return self.error_message.text_content()
+
+    def click_forgot_password(self):
+        self.forgot_password_link.click()
+
+    def is_page_loaded(self):
+        return self.page_title.is_visible() and self.login_button.is_visible()
+
     # ---------- Assertions ----------
     def assert_login_error(self):
         expect(self.error_message).to_be_visible()
+
+    def assert_page_loaded(self):
+        expect(self.page_title).to_be_visible()
+        expect(self.login_button).to_be_visible()
